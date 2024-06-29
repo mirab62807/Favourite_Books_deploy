1491f2d5b05432e8df49f555d823643d599d9061
mirab62807 <mirab62807@gmail.com>
2024-06-29 15:41:30 +0100

    corrected

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/env bash
+#!/usr/bin/myDjangoEnv bash
 # exit on error
 
 set -o errexit
 
 pip install -r requirements.txt
-python manage.py collecstatic -no-input
+python manage.py collectstatic -no-input
 python manage.py migrate
